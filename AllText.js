6bd52b0abadb5c8ba1959fcf19f49cd30f70c6ff
colin <colin.steele20@gmail.com>
2016-12-29 21:39:08 -0500

    cleaned content script and got names recognized

--- AllText.js
+++ AllText.js
@@ -1,24 +1,53 @@
-window.onload = function() {
-  AllText.init(); 
+function getNames(response){
+    var names = []
+
+    response.entities.forEach(function(entity){
+        if (entity.type == 'Person'){
+            names.push(entity.text)
+        }
+    });
+    return names;
 }
 
-class AllText {
+function tagNames(names){
+    allText = document.body.innerHTML;
 
-  var textBody = "";
-  
-  static init(){
-    var pTags = document.getElementsByTagName('p');
-    for(var p in pTags){
-      textBody = textBody + (p.innerHTML);
+    names.forEach(function(name){
+       console.log("doing a thing with "+name);
+       allText = allText.replace(name, "<span style='color: DeepPink' class='test'>"+name+"</span>"); 
+    });
+    document.body.innerHTML = allText; 
+}
+
+$(document).ready(function(){
+  allText = document.body.innerHTML  
+  pageUrl = document.URL
+
+  alchemyUrl = 'https://gateway-a.watsonplatform.net/calls/url/URLGetRankedNamedEntities?apikey='
+  alchemyKey = '6c8944b20948847c3923961cac83b128fe1dbd03'
+
+  $.ajax({
+    url: (alchemyUrl + alchemyKey),
+    data: {
+      'url': alchemyUrl,
+      'outputMode' : 'json',
+      'extract' : 'entities',
+      'sentiment' : '1',
+      'maxRetrieve' : '24',
+      'url' : pageUrl
+    },
+    cache: false,
+    type: 'POST',
+    success: function(response){
+        var names = getNames(response);
+        console.dir(names);
+        tagNames(names);
+    },
+    error: function(xhr){
+      console.dir(xhr)
     }
-  }
- 
-  static sendTextToExtension(){
-    
-  }
-  
-  static giveNamesSpanTag(name){
-	   
-  }
+  });
 
-}
+
+});
+
