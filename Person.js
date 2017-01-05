97c66c3661d62e521cd89cd89fa773bc7d20ca7b
colin <colin.steele20@gmail.com>
2017-01-05 10:11:36 -0500

    streamline element creation

--- Person.js
+++ Person.js
@@ -1,6 +1,6 @@
 function Person(name){
     this.name = name;  
-    this.nameOccuranceCount = 0; 
+    this.nameOccurances = [];
   
     this.tagOccurances = function(){
         this.giveSpans();
@@ -10,32 +10,57 @@ function Person(name){
     this.giveSpans = function(){
         var textBody = document.body.innerHTML;
         taggedName = 
-            "<span id='" + this.name + this.nameOccuranceCount +
+            "<span id='" + this.name + (this.nameOccurances.size) +
             "' class='" + this.name +
             "' style='color: DeepPink'>" +
             this.name +
             "</span>"
         textBody = textBody.replace(new RegExp(this.name, 'g'), taggedName);
-        this.nameOccuranceCount++;
+        if $("#"this.name, (this.nameOccurances.size))&& this.nameOccurances.push($("#"this.name, (this.nameOccurances.size)))
         document.body.innerHTML = textBody; 
     }
   
     this.givePortraits = function(){
         var frame = document.createElement('div');
+        frame.className =  this.name + '_portrait';  
         frame.style.width = '200px';
         frame.style.height = '200px';
-        frame.style.position = 'absolute';
         frame.style.top = '0px';
         frame.style.left = '0px';
-        frame.style.background = 'red';
-        frame.className =  this.name + '_portrait';  
+        frame.style.position = 'absolute';
+        frame.style.background = '#fc6';
+        frame.style.borderRadius = '3px';
         document.body.appendChild(frame);
+        //consider the following instead
+        $('<div />', {
+            'class' :  
+            'css' : {
+                'width' : 'lots'; 
+            }  
+            'mouseover' : function(e){
+                console.log(e+'yeah!');
+            } 
+        }).appendTo('body');
         //add positioning, color, rounded edges, mmaybe even a little pointer to the mouse
         //also figure out where to appendChild
+
+        //this one follows mouse movement
         $(document).on('mousemove', function(e){
-            frame.style.left = e.pageX + 'px';
-            frame.style.top = e.pageY + 'px';
+            //frame.style.left = e.pageX + 'px';
+            //frame.style.top = e.pageY + 'px';
         });
+
+        //this one sits above the name during mouseover on name
+        $(this.namespaninstance).on('mousover', function(e){
+            $(this).show;
+            this.putPortrait(); //if !already_put
+        });
+        
+        $(this.n).on('mouseout', function(e){
+            $(this).hide
+        });
+
+
     }
 
     this.getPortrait = function(){
@@ -60,6 +85,5 @@ function Person(name){
                 });
             } 
         });  
-    
     }
 }
